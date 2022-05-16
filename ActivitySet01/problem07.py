3eb7322f369aff7505aab080da8a856bd5958eb2
Nithin HD <96277732+NithinHD@users.noreply.github.com>
2022-05-16 09:19:08 +0000

    new

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,27 @@
-# Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+[2:47 pm, 16/05/2022] Veg Muzan: def computepay(h, r):
+    if h<=40:
+        return h*r
+    elif h>40:
+        h1=h-40
+        fee=40*r+h1*r*1.5
+        return fee
+hrs = float(input("Enter hours? "))
+rte = float(input("Enter rate per hour? "))
+p = computepay(hrs, rte)
+print("Pay", p)
+[2:48 pm, 16/05/2022] Veg Muzan: large=0
+small=1111111111111111110
+a= None
+while True:
+    a=input("Enter the number: ")
+    if a == 'Done' or a == 'done':
+        break
+    try:
+        if int(a)>0 or int(a) <=0:
+            if int(a)>large:
+                 large=int(a)
+            if int(a)<small:
+                 small=int(a)
+    except:
+        pass    
+print("Maximum =",large,"\nMinimum=",small)
--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,17 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+[2:48 pm, 16/05/2022] Veg Muzan: text= "X-DSPAM-Confidence:    0.8475"
+a=text.find("0")
+text=text[a:]
+print(float(text))
+[2:48 pm, 16/05/2022] Veg Muzan: fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+total=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    count=count+1
+    l=line.find("0")
+    number=float(line[l:])
+    total=total+number
+avg=total/count
+print("Average spam confidence:",avg)
